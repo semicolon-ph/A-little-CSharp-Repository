b5da51c15bbe8511717ee07e85576aab75ffa9d8
Semicolon <jmbmahinan.student@jrcamanila.edu.ph>
2026-05-30 12:45:13 +0800

    Introduction

---
+++ ConsoleApplications/Introduction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Introduction
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+            Console.WriteLine("My name is Semicolon!");
+            Console.WriteLine("My Sexuality is Bisexual!");
+            Console.WriteLine("My age is 13");
+            Console.WriteLine("I am a Male");
+        }
+    }
+}
