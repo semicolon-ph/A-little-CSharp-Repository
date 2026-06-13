b5212253583a3be172dd5a6307f47d32396142a4
Semicolon <jmbmahinan.student@jrcamanila.edu.ph>
2026-06-13 11:09:17 +0800

    forgot to name this Read Name so yeah :/

---
+++ ConsoleApplications/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_Tutorial__ZK_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi! what is your name? Write and Press Enter to Continue");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Hello there, " + Name + "! my name is Mr. Tutorial Man!");
+            Console.ReadKey();
+        }
+    }
+}
